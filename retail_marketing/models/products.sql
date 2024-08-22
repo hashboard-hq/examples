535fddd5817d3973accdcb7fef106f70ae7c43fc
Carlos Aguilar <carlos@Hashs-Air.localdomain>
2024-08-22 11:06:45 -0400

    complete bigquery project

--- retail_marketing/models/products.sql
+++ retail_marketing/models/products.sql
@@ -1,8 +1,8 @@
 select 
-  cast(hash("pizza_type" || "pizza_shape" || "pizza_size" ) as string) as "id",
-  "pizza_size",
-  "pizza_shape",
-  "pizza_type",
-  "Price"
+  cast(FARM_FINGERPRINT(pizza_type || pizza_shape || pizza_size ) as string) as id,
+  pizza_size,
+  pizza_shape,
+  pizza_type,
+  price
 from {{ source("generated_sources", "sales_data") }}
 group by all
--- retail_marketing/models/sales.sql
+++ retail_marketing/models/sales.sql
@@ -1,10 +1,10 @@
 select 
-  hash("order_id" || '_' || "item_number") as "item_id",
-  "order_id",
-  "item_number",
-  cast(hash("pizza_type" || "pizza_shape" || "pizza_size" ) as string) as "product_id",
-  "quantity",
-  "customer_id",
-  cast("item_price" as decimal) as "item_price",
-  "timestamp"
+  FARM_FINGERPRINT(order_id || '_' || item_number) as item_id,
+  order_id,
+  item_number,
+  cast(FARM_FINGERPRINT(pizza_type || pizza_shape || pizza_size ) as string) as product_id,
+  quantity,
+  customer_id,
+  cast(item_price as decimal) as item_price,
+  timestamp
 from {{ source("generated_sources", "sales_data") }}
